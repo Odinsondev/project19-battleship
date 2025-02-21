a802d18622346c3446c1fae3c596590dbe93e705
Odinsondev <122601475+Odinsondev@users.noreply.github.com>
2025-02-20 19:07:26 -0800

    Initial dist subtree commit

--- webpack.config.js
+++ webpack.config.js
@@ -2,21 +2,21 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
-    clean: true
+    clean: true,
   },
 
   devtool: 'inline-source-map',
 
   plugins: [
     new HtmlWebpackPlugin({
-        template: './src/index.html',
-        filename: 'index.html',
-        inject: 'body'
+      template: './src/index.html',
+      filename: 'index.html',
+      inject: 'body',
     }),
   ],
 
